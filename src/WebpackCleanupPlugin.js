fbd3ca378ee9a24a71a57e40b8288b204286c878
domehead100 <4917311+domehead100@users.noreply.github.com>
2018-05-23 04:26:54 -0400

    Update to new tapable hook api

--- src/WebpackCleanupPlugin.js
+++ src/WebpackCleanupPlugin.js
@@ -14,7 +14,7 @@ class WebpackCleanupPlugin {
   apply(compiler) {
     const outputPath = compiler.options.output.path;
 
-    compiler.plugin('done', (stats) => {
+    compiler.hook.done.tap('done', (stats) => {
       if (compiler.outputFileSystem.constructor.name !== 'NodeOutputFileSystem') {
         return;
       }
